15a6420b7eaf6e55792e8749fc31ff18a9ca71a5
marisasetya <marisasetya11@gmail.com>
2023-06-05 21:17:18 +0700

    Revert "login dengan API"
    This reverts commit ea91d1564798bd1dc85a2483d17c833efa87305b.

--- app/src/main/java/com/example/user/navigationdrawersample/Auth/ApiServices.java
+++ app/src/main/java/com/example/user/navigationdrawersample/Auth/ApiServices.java
@@ -1,11 +1,7 @@
 package com.example.user.navigationdrawersample.Auth;
 
 public class ApiServices {
-    private static String HOST = "http://192.168.18.23:8000";
+    private static String HOST = "http://192.168.43.199:8000";
     private static String API = HOST + "/api/";
-    private static String API_LOGIN = HOST+"/api/login";
 
-    public static String getApiLogin() {
-        return API_LOGIN;
-    }
 }
