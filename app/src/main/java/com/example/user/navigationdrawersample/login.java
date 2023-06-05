7001745a8bede369c49afdb5a8fb6a831b1b4c6a
aishandavaniaa <109503610+aishandavaniaa@users.noreply.github.com>
2023-06-05 21:55:15 +0700

    update login

--- app/src/main/java/com/example/user/navigationdrawersample/login.java
+++ app/src/main/java/com/example/user/navigationdrawersample/login.java
@@ -2,13 +2,32 @@ package com.example.user.navigationdrawersample;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class login extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.user.navigationdrawersample.Auth.ApiServices;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class login extends AppCompatActivity implements View.OnClickListener {
     Button buttonLogin;
+    private EditText et_email,et_password;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -16,18 +35,79 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        et_email = (EditText) findViewById(R.id.etl_email);
+        et_password = (EditText) findViewById(R.id.etl_password);
         buttonLogin = (Button) findViewById(R.id.btn_login);
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent toHome = new Intent(login.this, MainActivity.class);
-                startActivity(toHome);
-                finish();
-            }
-        });
+        buttonLogin.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View view) {
+       if(view == buttonLogin) {
+           String email = et_email.getText().toString().trim();
+           String pass = et_password.getText().toString().trim();
+
+           if (!email.isEmpty() && !pass.isEmpty()){
+               Login(email,pass);
+           }else {
+               et_email.setError("Masukkan Email!");
+               et_password.setError("Masukkan Password!");
+           }
+       }
+    }
+    private void Login(final String email, final String password) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ApiServices.getApiLogin(),
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+//                            String value = jsonObject.getString("value");
+                            String pesan = jsonObject.getString("message");
+
+                            if (pesan.equals("success")) {
+                                SharedPreferences sharedPreferences = login.this.getSharedPreferences("PHITIX", MODE_PRIVATE);
+                                Toast.makeText(login.this, "Login Sukses!", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(login.this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(login.this, "Gagal Login" + pesan, Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(login.this, "Login Error" + e.toString(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                },
+                    new Response.ErrorListener(){
+                        @Override
+                        public void onErrorResponse(VolleyError error){
+                        Toast.makeText(login.this, "Error" + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                    })
+
+        {
+
+            protected Map<String,String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+
+            }
+        };
+
+    RequestQueue requestQueue = Volley.newRequestQueue(this);
+    requestQueue.add(stringRequest);
+
+        }
+
 }
-
-
-
